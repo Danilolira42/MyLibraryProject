a46ea1eb6bd98701216e9a101bea308bde25f1f3
MP - Danilo Lira da Silva <danilo.silva@micropower.com.br>
2025-09-17 18:18:12 -0300

    Adicionar arquivos de projeto.

---
+++ MyLibrary.API/Controllers/LibraryController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyLibrary.API.ModelDTO;
+
+namespace MyLibrary.API.Controllers;
+[Route("GetLibrary")]
+[ApiController]
+public class LibraryController : ControllerBase
+{
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+
+    public IActionResult GetLibrary(string request)
+    {
+        var Books = new List<Books>();
+
+        return Ok();
+    }
+
+    [HttpGet("GetBooksById/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<Books>> GetBookById(int id)
+    {
+        var BooksById = new Books();
+
+        return Ok(BooksById);
+    }
+
+    [HttpPost("CreateBooks")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+
+    public IActionResult CreateBooks(Books request)
+    {
+
+        return Ok(request);
+
+    }
+
+    [HttpPut("{Id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+
+    public IActionResult UpdateBooks(string request)
+    {
+        var updateBooks = new Books();
+
+        return NoContent();
+
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+
+    public IActionResult DeleteBooks(int Id)
+    {
+
+        var deleteBooks = new Books();
+
+        deleteBooks.Id = Id;
+        deleteBooks.Title = "Notebook";
+        deleteBooks.Author = "Dell";
+        deleteBooks.Price = 5000;
+        deleteBooks.Stock = 10;
+
+        return Ok($"Produto {Id} excluído");
+
+    }
+}
---
+++ MyLibrary.API/ModelDTO/Books.cs
@@ -0,0 +1,17 @@
+﻿namespace MyLibrary.API.ModelDTO;
+
+public class Books
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Author { get; set; } = string.Empty;
+
+    public float Price { get; set; }
+
+    public int Stock { get; set; }
+
+    public string Genre { get; set; } = string.Empty;
+
+}
